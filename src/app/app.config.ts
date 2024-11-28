07ac5b34449b141ae23c068cc9056486493484c0
Mufil Rahman A <mufil.r@simplicontract.com>
2024-11-29 00:48:08 +0530

    installing store,effect,signal,devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, provideExperimentalZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 
 export const appConfig: ApplicationConfig = {
@@ -9,6 +12,8 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
- 
-  ]
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
